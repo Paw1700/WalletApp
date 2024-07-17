1695d9cb616730630a506e605c1c1b3d8803df50
Paw1700 <uryckip@gmail.com>
2024-07-17 14:17:37 +0200

    FIX - routes to assets in manifest

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideAnimations(), provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideAnimations(), 
+    provideRouter(routes), 
+    provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
-          })]
+    }),
+    provideHttpClient()
+  ]
 };
