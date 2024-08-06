de6a5c95b76b575257df68b61f0487978a99754c
Paw1700 <uryckip@gmail.com>
2024-08-06 14:43:42 +0200

    CHANGE - sorted banks.json and categories.json

--- src/app/pages/add_transaction/add_transaction.page.ts
+++ src/app/pages/add_transaction/add_transaction.page.ts
@@ -1,13 +1,15 @@
 import { Component } from "@angular/core";
 import { TransactionTypeChooseBar } from "../../components/transaction_type_choose_bar/transaction_type_chosse_bar.component";
 import { CategoryChooseBar } from "../../components/category_choose_bar/category_choose_bar.component";
+import { NumberInputComponent } from "../../components/number_input_bar/number_input_bar.component";
 
 @Component({
     selector: 'add_transaction_page',
     standalone: true,
     imports: [
         TransactionTypeChooseBar,
-        CategoryChooseBar
+        CategoryChooseBar,
+        NumberInputComponent
     ],
     templateUrl: './add_transaction.page.html',
     styleUrl: './add_transaction.page.scss'
