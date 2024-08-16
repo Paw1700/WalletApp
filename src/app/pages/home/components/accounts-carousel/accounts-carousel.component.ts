e4a3e99a1e11bf31217d594704cb9c973b2c7c1d
Paw1700 <uryckip@gmail.com>
2024-08-16 18:59:38 +0200

    CHANGE - Home Page - Scrool Accounts Tabs Method

--- src/app/pages/home/components/accounts-carousel/accounts-carousel.component.ts
+++ src/app/pages/home/components/accounts-carousel/accounts-carousel.component.ts
@@ -18,54 +18,23 @@ export class AccountsCarousel {
     @Input() accounts_data: AccountBar_Data[] = []
     @Output() active_account_number = new EventEmitter<number>()
 
-    scroll_position = 0
+    scroll_value = 0
     active_list_indicator = -1
     show_list_indicator = false
-    private start_touch_position = { x: 0, y: 0 }
-    private end_touch_posistion = { x: 0, y: 0 }
-    private readonly DIFF_Y_LIMIT = 50
-    private readonly DIFF_X_TRIGGER_POINT = 30
 
-    handleSwipeGesture(e: TouchEvent, type: 'start' | 'end') {
-        switch (type) {
-            case 'start':
-                this.start_touch_position.x = e.changedTouches[0].clientX
-                this.start_touch_position.y = e.changedTouches[0].clientY
-                break
-            case 'end':
-                this.end_touch_posistion.x = e.changedTouches[0].clientX
-                this.end_touch_posistion.y = e.changedTouches[0].clientY
-                this.swipeCarousel()
-                break
-        }
+    handleScrollGesture(e: any) {
+        this.scroll_value = e.target.scrollLeft
     }
 
-    private swipeCarousel() {
-        const position_x_diff = this.start_touch_position.x - this.end_touch_posistion.y
-        const position_y_diff = this.start_touch_position.y - this.end_touch_posistion.y
-
-        if (Math.abs(position_y_diff) < this.DIFF_Y_LIMIT) {
-            if (position_x_diff > this.DIFF_X_TRIGGER_POINT) {// SWIPE TO LEFT <--- 
-                if (this.scroll_position >= 0 && this.scroll_position < this.accounts_data.length * 100) {
-                    this.scroll_position += 100
-                    this.informOfAccountChange()
-                }
-            }
-            if (position_x_diff < -this.DIFF_X_TRIGGER_POINT) { // SWIPE TO RIGHT -->
-                if (this.scroll_position > 0 && this.scroll_position <= this.accounts_data.length * 100) {
-                    this.scroll_position -= 100
-                    this.informOfAccountChange()
-                }
-            }
-        }
+    handleTouchEnd() {
+        const scroll_value_divided_by_window_width_rounded_to_integer = Math.round(this.scroll_value / window.innerWidth)
+        console.log(scroll_value_divided_by_window_width_rounded_to_integer);
+        document.getElementById('CAROUSEL')?.scrollTo({ left: scroll_value_divided_by_window_width_rounded_to_integer * window.innerWidth, top: 0, behavior: 'smooth' })
+        this.informOfAccountChange(scroll_value_divided_by_window_width_rounded_to_integer - 1)
     }
 
-    private informOfAccountChange() {
-        let account_number = this.active_list_indicator = (this.scroll_position / 100) - 1
+    private informOfAccountChange(account_list_number: number) {
+        let account_number = this.active_list_indicator = account_list_number
         this.active_account_number.emit(account_number)
-        this.show_list_indicator = true
-        setTimeout(() => {
-            this.show_list_indicator = false
-        }, 500);
     }
 }
